376c246daadfcb4e08a39cd7561793dba5eda0ac
unknown <jagudelos@intergrupo.com>
2020-02-28 07:05:05 -0500

    facebook

---
+++ src/test/java/starter/navigation/pages/facebook.java
@@ -0,0 +1,7 @@
+package starter.navigation.pages;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://www.facebook.com/")
+public class facebook extends PageObject {}
---
+++ src/test/java/starter/navigation/pages/publicion_facebook.java
@@ -0,0 +1,7 @@
+package starter.navigation.pages;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://www.facebook.com/MefrupBike/videos/470148463869461/")
+public class publicion_facebook extends PageObject {}
