be0535c31f70ab4f6599cbdf2116e610a0e84899
juan esteban agudelo sierra <juanes0325@iMac-de-juan.local>
2020-02-28 09:45:22 -0500

    dh

--- build/resources/test/features/test/test_example.feature
+++ build/resources/test/features/test/test_example.feature
@@ -3,4 +3,7 @@ Feature: login_test_serenity_hakeadon
   Scenario: login test
     Given "Sergey" ejemplo test
     When el buscan "cucumber"
+    And validar tablas
     Then validar titulos "$17,800"
+
+
--- src/test/java/starter/stepdefinitions/ejemplo_test.java
+++ src/test/java/starter/stepdefinitions/ejemplo_test.java
@@ -4,7 +4,6 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import net.serenitybdd.core.Serenity;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.serenitybdd.screenplay.ensure.Ensure;
@@ -12,10 +11,7 @@ import starter.model.dashBoard.dashboard;
 import starter.navigation.NavigateTo;
 import starter.model.dashBoard.OverViewData_DashBoard;
 import starter.model.menu.OverViewData_menu;
-import starter.search.AddATodoItem;
 import starter.task.Builder.login_Builder_patron;
-import starter.task.StepBuilder.user_login_task;
-
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
@@ -50,6 +46,44 @@ String pass;
         );
     }
 
+    @When("validar tabla")
+    public void validar_tablas() {
+        theActorCalled(user).attemptsTo(
+                
+                Ensure.that(dashboard.TableRow_1_CELL_1).isDisplayed(),
+                Ensure.that(dashboard.TableRow_1_CELL_2).isDisplayed(),
+                Ensure.that(dashboard.TableRow_1_CELL_3).isDisplayed(),
+                Ensure.that(dashboard.TableRow_1_CELL_4).isDisplayed(),
+                Ensure.that(dashboard.TableRow_1_CELL_5).isDisplayed(),
+                Ensure.that(dashboard.TableRow_2_CELL_1).isDisplayed(),
+                Ensure.that(dashboard.TableRow_2_CELL_2).isDisplayed(),
+                Ensure.that(dashboard.TableRow_2_CELL_3).isDisplayed(),
+                Ensure.that(dashboard.TableRow_2_CELL_4).isDisplayed(),
+                Ensure.that(dashboard.TableRow_2_CELL_5).isDisplayed(),
+                Ensure.that(dashboard.TableRow_3_CELL_1).isDisplayed(),
+                Ensure.that(dashboard.TableRow_3_CELL_2).isDisplayed(),
+                Ensure.that(dashboard.TableRow_3_CELL_3).isDisplayed(),
+                Ensure.that(dashboard.TableRow_3_CELL_4).isDisplayed(),
+                Ensure.that(dashboard.TableRow_3_CELL_5).isDisplayed(),
+                Ensure.that(dashboard.TableRow_4_CELL_1).isDisplayed(),
+                Ensure.that(dashboard.TableRow_4_CELL_2).isDisplayed(),
+                Ensure.that(dashboard.TableRow_4_CELL_3).isDisplayed(),
+                Ensure.that(dashboard.TableRow_4_CELL_4).isDisplayed(),
+                Ensure.that(dashboard.TableRow_4_CELL_5).isDisplayed(),
+                Ensure.that(dashboard.TableRow_5_CELL_1).isDisplayed(),
+                Ensure.that(dashboard.TableRow_5_CELL_2).isDisplayed(),
+                Ensure.that(dashboard.TableRow_5_CELL_3).isDisplayed(),
+                Ensure.that(dashboard.TableRow_5_CELL_4).isDisplayed(),
+                Ensure.that(dashboard.TableRow_5_CELL_5).isDisplayed(),
+                Ensure.that(dashboard.TableRow_6_CELL_1).isDisplayed(),
+                Ensure.that(dashboard.TableRow_6_CELL_2).isDisplayed(),
+                Ensure.that(dashboard.TableRow_6_CELL_3).isDisplayed(),
+                Ensure.that(dashboard.TableRow_6_CELL_4).isDisplayed(),
+                Ensure.that(dashboard.TableRow_6_CELL_5).isDisplayed()
+        );
+    }
+
+
     @Then("validar titulos {string}")
     public void validate_titles(String validate) {
 
@@ -63,68 +97,9 @@ theActorInTheSpotlight().should (
         ,seeThat ("prueba de visualizacion opción del menu loans",OverViewData_menu.LOANS (),equalTo ("Loans"))
         ,seeThat ("prueba de visualizacion opción del menu mortage",OverViewData_menu.MORTAGAGES (),equalTo ("Mortgages"))
 );
-        theActorCalled(user).attemptsTo(
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 1 columna 1"),
-                 Ensure.that(dashboard.TableRow_1_CELL_1).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 1 columna 2"),
-                Ensure.that(dashboard.TableRow_1_CELL_2).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 1 columna 3"),
-                Ensure.that(dashboard.TableRow_1_CELL_3).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 1 columna 4"),
-                Ensure.that(dashboard.TableRow_1_CELL_4).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 1 columna 5"),
-                Ensure.that(dashboard.TableRow_1_CELL_5).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 2 columna 1"),
-                Ensure.that(dashboard.TableRow_2_CELL_1).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 2 columna 2"),
-                Ensure.that(dashboard.TableRow_2_CELL_2).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 2 columna 3"),
-                Ensure.that(dashboard.TableRow_2_CELL_3).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 2 columna 4"),
-                Ensure.that(dashboard.TableRow_2_CELL_4).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 2 columna 5"),
-                Ensure.that(dashboard.TableRow_2_CELL_5).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 3 columna 1"),
-                Ensure.that(dashboard.TableRow_3_CELL_1).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 3 columna 2"),
-                Ensure.that(dashboard.TableRow_3_CELL_2).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 3 columna 3"),
-                Ensure.that(dashboard.TableRow_3_CELL_3).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 3 columna 4"),
-                Ensure.that(dashboard.TableRow_3_CELL_4).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 3 columna 5"),
-                Ensure.that(dashboard.TableRow_3_CELL_5).isDisplayed(),
-                //AddATodoItem.called("prueba de visualizacion tabla fila 4 columna 1"),
-                Ensure.that(dashboard.TableRow_4_CELL_1).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 4 columna 2"),
-                Ensure.that(dashboard.TableRow_4_CELL_2).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 4 columna 3"),
-                Ensure.that(dashboard.TableRow_4_CELL_3).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 4 columna 4"),
-                Ensure.that(dashboard.TableRow_4_CELL_4).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 4 columna 5"),
-                Ensure.that(dashboard.TableRow_4_CELL_5).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 5 columna 1"),
-                Ensure.that(dashboard.TableRow_5_CELL_1).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 5 columna 2"),
-                Ensure.that(dashboard.TableRow_5_CELL_2).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 5 columna 3"),
-                Ensure.that(dashboard.TableRow_5_CELL_3).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 5 columna 4"),
-                Ensure.that(dashboard.TableRow_5_CELL_4).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 5 columna 5"),
-                Ensure.that(dashboard.TableRow_5_CELL_5).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 6 columna 1"),
-                Ensure.that(dashboard.TableRow_6_CELL_1).isDisplayed(),
-                //AddATodoItem.called("prueba de visualizacion tabla fila 6 columna 2"),
-                Ensure.that(dashboard.TableRow_6_CELL_2).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 6 columna 3"),
-                Ensure.that(dashboard.TableRow_6_CELL_3).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 6 columna 4"),
-                Ensure.that(dashboard.TableRow_6_CELL_4).isDisplayed(),
-                //AddATodoItem.called ("prueba de visualizacion tabla fila 6 columna 5"),
-                Ensure.that(dashboard.TableRow_6_CELL_5).isDisplayed()
-        );
+
     }
+
+
 
 }
