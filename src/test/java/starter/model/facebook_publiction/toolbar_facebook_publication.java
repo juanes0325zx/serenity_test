f3b111b73822c494c064ba77cd9cb472ba5f5be2
juan esteban agudelo sierra <juanes0325@iMac-de-juan.local>
2020-03-03 15:49:57 -0500

    botonera de directos

--- src/test/java/starter/model/facebook_publiction/toolbar_facebook_publication.java
+++ src/test/java/starter/model/facebook_publiction/toolbar_facebook_publication.java
@@ -8,4 +8,10 @@ public class toolbar_facebook_publication {
     public static By comentar = By.xpath("//div[@class='_6iiu']//span[@class=' _18vi']//div[@class=' _666h  _18vj _18vk _42ft']");
     public static By compartir = By.xpath("//div[@class='_6iiu']//span[@class=' _18vi']//div[@class=' _2nj7  _18vj _18vk']");
 
+    public static By like_btn_directo = By.xpath("//div[@class='_6599']//div[@class='_3399 _a7s _20h6 _610i _610j _125r _2h27 clearfix _zw3']//span[@class='_1mto']//div[@class='_khz _4sz1 _4rw5 _3wv2']");
+    public static By comentar_btn_directo = By.xpath("//div[@class='_6599']//div[@class='_3399 _a7s _20h6 _610i _610j _125r _2h27 clearfix _zw3']//span[@class='_1mto']//span[@class='_6a _15-7 _3h-u _4k43']");
+    public static By compartir_btn_directo = By.xpath("//div[@class='_6599']//div[@class='_3399 _a7s _20h6 _610i _610j _125r _2h27 clearfix _zw3']//span[@class='_1mto']//div[@behaviors]");
+
+
+
 }
--- src/test/resources/features/test/facebookLoginAndLike.feature
+++ src/test/resources/features/test/facebookLoginAndLike.feature
@@ -4,10 +4,10 @@ Feature: createUser
 
 
     Given "<email>" login user "<password>"
-    When "<Publication>" publicacion a darle like
+    When "<type>" publicacion a darle like
     Then  serra sección
 
 
     Examples:
-      | email                  | password         | Publication      |
-      | juanes9444@hotmail.com | 1994juanes | https://www.facebook.com/photo.php?fbid=10219983675638494&set=a.4219660566042&type=3&theater |
+      | email                  | password         | type      |
+      | juanes9444@hotmail.com | 1994juanes | 1 |
